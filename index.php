86d960f5489ea413f70311f0c973e3a5bacc671a
philippft <dzosfphilip@gmail.com>
2025-03-04 16:20:36 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,11 @@
 <body>
     <h1>Tetsing git 2</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus nobis obcaecati harum quod molestias rem reprehenderit neque aperiam ipsum atque iste, delectus aspernatur dicta id voluptas dolores deleniti quasi eum.</p>
+
+    <ul>
+        <li>list 1</li>
+        <li>list 2</li>
+        <li>list 3</li>
+    </ul>
 </body>
 </html>
